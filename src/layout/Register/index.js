6cda8a03236f39d754b2330cddc5402d4ecddf0a
iMMoCodes <74028194+iMMoCodes@users.noreply.github.com>
2021-03-17 15:00:32 +0200

    added necessary fields to form and styled them

--- src/layout/Register/index.js
+++ src/layout/Register/index.js
@@ -1,22 +1,59 @@
 import React from 'react'
-import { Button, Form } from 'semantic-ui-react'
+import { Button, Form, Grid, Header as SemanticHeader, Segment } from 'semantic-ui-react'
 import Header from '../../components/Header/index'
 
 const RegisterUI = () => {
 	return (
 		<div>
 			<Header />
-			<Form>
-				<Form.Field>
-					<label>First Name</label>
-					<input placeholder='First Name' />
-				</Form.Field>
-				<Form.Field>
-					<label>Last Name</label>
-					<input placeholder='Last Name' />
-				</Form.Field>
-				<Button type='submit'>Submit</Button>
-			</Form>
+			<Grid centered>
+				<Grid.Column style={{ maxWidth: 550, marginTop: 20 }}>
+					<SemanticHeader>Sign up</SemanticHeader>
+					<Segment>
+						<Form>
+							<Form.Field>
+								<Form.Input
+									placeholder='Username'
+									label='Username'
+								/>
+							</Form.Field>
+							<Form.Field>
+								<Form.Input
+									placeholder='First Name'
+									label='First Name'
+								/>
+							</Form.Field>
+							<Form.Field>
+								<Form.Input
+									placeholder='Last Name'
+									label='Last Name'
+								/>
+							</Form.Field>
+							<Form.Field>
+								<Form.Input
+									type='email'
+									placeholder='Email'
+									label='Email'
+								/>
+							</Form.Field>
+							<Form.Field>
+								<Form.Input
+									type='password'
+									placeholder='Password'
+									label='Password'
+								/>
+							</Form.Field>
+							<Button
+								primary
+								fluid
+								type='submit'
+							>
+								Submit
+							</Button>
+						</Form>
+					</Segment>
+				</Grid.Column>
+			</Grid>
 		</div>
 	)
 }

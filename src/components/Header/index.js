c47a4ffdb67895bfd3d3e4949cc207c0e5c012f8
iMMoCodes <74028194+iMMoCodes@users.noreply.github.com>
2021-03-17 14:47:19 +0200

    created form from semantic UI

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,27 +1,38 @@
 import React from 'react'
 import { Button, Icon, Image, Menu } from 'semantic-ui-react'
 import logo from '../../assets/images/Awesome-Tux.svg'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const Header = () => {
+	const { pathname } = useLocation()
 	return (
 		<Menu pointing>
 			<Image src={logo} width={50} />
 			<Menu.Item as={Link} to='/' style={{ fontSize: '2rem' }}>
 				iMMoContacts
 			</Menu.Item>
-			<Menu.Item position='right'>
-				<Button as={Link} to='/contacts/create' inverted color='green' icon>
-					<Icon name='address book'></Icon>
-					Create Contact
-				</Button>
-			</Menu.Item>
-			<Menu.Item>
-				<Button color='red' inverted icon>
-					<Icon name='log out'></Icon>
-					Logout
-				</Button>
-			</Menu.Item>
+			{pathname === '/' && (
+				<Menu.Item position='right'>
+					<Button
+						as={Link}
+						to='/contacts/create'
+						inverted
+						color='green'
+						icon
+					>
+						<Icon name='address book'></Icon>
+						Create Contact
+					</Button>
+				</Menu.Item>
+			)}
+			{pathname === '/' && (
+				<Menu.Item>
+					<Button color='red' inverted icon>
+						<Icon name='log out'></Icon>
+						Logout
+					</Button>
+				</Menu.Item>
+			)}
 		</Menu>
 	)
 }

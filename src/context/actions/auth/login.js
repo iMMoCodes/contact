285d532dc6733ab5749f5b7922f40de6b1995221
iMMoCodes <74028194+iMMoCodes@users.noreply.github.com>
2021-03-19 14:58:07 +0200

    used axios intercepter to authenticate token and log user out if not valid

--- src/context/actions/auth/login.js
+++ src/context/actions/auth/login.js
@@ -1,12 +1,13 @@
 import { LOGIN_ERROR, LOGIN_LOADING, LOGIN_SUCCESS } from '../../../constants/actionTypes'
-import axiosInstance from '../../../helpers/axios'
+import axiosInstance from '../../../helpers/axiosInstance'
 
 export const login = ({ password, username }) => (dispatch) => {
 	dispatch({
 		type: LOGIN_LOADING,
 	})
 
-	axiosInstance.post('/auth/login', { password, username })
+	axiosInstance()
+		.post('/auth/login', { password, username })
 		.then((res) => {
 			localStorage.token = res.data.token
 			dispatch({
--- src/context/actions/auth/register.js
+++ src/context/actions/auth/register.js
@@ -1,12 +1,13 @@
 import { REGISTER_ERROR, REGISTER_LOADING, REGISTER_SUCCESS } from '../../../constants/actionTypes'
-import axiosInstance from '../../../helpers/axios'
+import axiosInstance from '../../../helpers/axiosInstance'
 
 export const register = ({ email, password, username, firstName: first_name, lastName: last_name }) => (dispatch) => {
 	dispatch({
 		type: REGISTER_LOADING,
 	})
 
-	axiosInstance.post('/auth/register', { email, password, username, first_name, last_name })
+	axiosInstance()
+		.post('/auth/register', { email, password, username, first_name, last_name })
 		.then((res) => {
 			dispatch({
 				type: REGISTER_SUCCESS,

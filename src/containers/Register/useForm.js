e9f52cf9cbab83edcddc796c032bbb7290504fe2
iMMoCodes <74028194+iMMoCodes@users.noreply.github.com>
2021-04-09 14:52:38 +0300

    removed console log

--- src/containers/Register/useForm.js
+++ src/containers/Register/useForm.js
@@ -33,14 +33,8 @@ export default () => {
 	const onChange = (e, { name, value }) => {
 		setForm({ ...form, [name]: value })
 	}
-	console.log(`form`, form)
 
-	const registerFormValid =
-		!form.username?.length ||
-		!form.firstName?.length ||
-		!form.lastName?.length ||
-		!form.email?.length ||
-		!form.password?.length
+	const registerFormValid = !form.username?.length || !form.firstName?.length || !form.lastName?.length || !form.email?.length || !form.password?.length
 
 	const onSubmit = () => {
 		setFieldErrors({})
